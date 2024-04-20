fdcbf5a1059fef7338344060ab442dc90ce0ed16
Dmitry Bala <balaoleg24@gmail.com>
2024-04-20 17:00:55 +0200

    Commit message

---
+++ lesson8/script.js
@@ -0,0 +1,25 @@
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this;
+    },
+    down: function () {
+        this.step--;
+        return this;
+    },
+    showStep: function () {
+        console.log(this.step);
+        return this;
+    }
+};
+
+// ladder.up();
+
+// ladder.up();
+
+// ladder.down();
+
+// ladder.showStep(); // 1
+
+ladder.up().up().down().showStep(); // 1
