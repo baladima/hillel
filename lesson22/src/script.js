aee61e9f6d580a135ecc2656df002d6917a343d9
Dmitry Bala <balaoleg24@gmail.com>
2024-06-28 14:49:29 +0200

    Commit message

---
+++ lesson22/src/script.js
@@ -0,0 +1,54 @@
+import './style.scss';
+
+const reloadButton = document.querySelector("[data-reload-button]");
+const select = document.querySelector("[data-select]");
+
+reloadButton.addEventListener('click', () => {
+    updateWeather();
+})
+
+select.addEventListener('change', () => {
+    updateWeather();
+})
+
+function updateWeather() {
+    const city = document.querySelector("[data-select]").value;
+    const API_KEY = '1f2e850a04f56d2c12fab50327fffb0b';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const name = document.querySelector("[data-name]");
+            const temp = document.querySelector("[data-temp]");
+            const humidity = document.querySelector("[data-humidity]");
+            const windSpeed = document.querySelector("[data-wind-speed]");
+
+            switch (true) {
+                case (data.name === "Kyiv"):
+                    name.textContent = "Киев";
+                    break;
+                case (data.name === "Krakow"):
+                    name.textContent = "Краков";
+                    break;
+                case (data.name === "Nikopol"):
+                    name.textContent = "Никополь";
+                    break;
+                case (data.name === "London"):
+                    name.textContent = "Лондон";
+                    break;
+                case (data.name === "New York"):
+                    name.textContent = "Нью-Йорк";
+                    break;
+            }
+
+            temp.textContent = data.main.temp;
+            humidity.textContent = data.main.humidity;
+            windSpeed.textContent = data.wind.speed;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+updateWeather();
--- lesson22/webpack.config.js
+++ lesson22/webpack.config.js
@@ -1,10 +1,57 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src/app.js'),
+    entry: './src/script.js',
     output: {
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.min.js',
     },
-}
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ],
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            minimize: true,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+        watchContentBase: true,
+        progress: true,
+    },
+};
